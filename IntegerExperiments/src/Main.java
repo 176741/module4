f98ac97a65afd84be0ea96d6436ce5fd22c29e26
Никита <mr.baygushkin@mail.ru>
2020-02-16 22:14:33 +0300

    2 homework - MIN & MAX values

--- IntegerExperiments/src/Main.java
+++ IntegerExperiments/src/Main.java
@@ -17,4 +17,30 @@ public class Main
         System.out.println(sum);
         return sum;
     }
+    public static void minMax(){
+        int intMin = Integer.MIN_VALUE;
+        System.out.println("Минимальное значение int: " + intMin);
+        int intMax = Integer.MAX_VALUE;
+        System.out.println("Максимальное значение int: " + intMax);
+        double doubleMin = Double.MIN_VALUE;
+        System.out.println("Минимальное значение double: " + doubleMin);
+        double doubleMax = Double.MAX_VALUE;
+        System.out.println("Максимальное значение double: " + doubleMax);
+        float floatMin = Float.MIN_VALUE;
+        System.out.println("Минимальное значение float: " + floatMin);
+        float floatMax = Float.MAX_VALUE;
+        System.out.println("Максимальное значение float: " + floatMax);
+        byte byteMin = Byte.MIN_VALUE;
+        System.out.println("Минимальное значение byte: " + byteMin);
+        byte byteMax = Byte.MAX_VALUE;
+        System.out.println("Максимальное значение byte: " + byteMax);
+        short shortMin = Short.MIN_VALUE;
+        System.out.println("Минимальное значение short: " + shortMin);
+        short shortMax = Short.MAX_VALUE;
+        System.out.println("Максимальное значение short: " + shortMax);
+        long longMin = Long.MIN_VALUE;
+        System.out.println("Минимальное значение long: " + longMin);
+        long longMax = Long.MAX_VALUE;
+        System.out.println("Максимальное значение long: " + longMax);
+    }
 }
