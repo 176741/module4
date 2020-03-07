c260abc0afc9ca8ed90d019068d160a240cf99b4
Никита <mr.baygushkin@mail.ru>
2020-03-07 11:44:37 +0300

    6 homework - Date & Calendar methods with before method

--- StringExperiments/src/Loader.java
+++ StringExperiments/src/Loader.java
@@ -2,6 +2,11 @@ import org.w3c.dom.ls.LSOutput;
 
 import javax.crypto.spec.PSource;
 import javax.lang.model.type.NullType;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Loader
@@ -55,6 +60,22 @@ public class Loader
             System.out.println("Фамилия: " + lastName);
             System.out.println("Имя: " + firstName);
             System.out.println("Отчество: " + middleName);
+        }
+        //Домашняя работа 4.6
+        //задание 1 - Написать программу, которая будет распечатывать Ваш возраст и соответствующий ему день рождения и день недели до текущего момента времени
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy EEEE", new Locale("ru"));
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(1994, Calendar.MAY, 02);
+        Date date = new Date();
+        Date birthday = calendar.getTime();
+        int i = 0;
+        while (date.compareTo(birthday) > 0)
+        {
+            System.out.println(i + " - " + dateFormat.format(birthday));
+            calendar.add(Calendar.YEAR, 1);
+            birthday = calendar.getTime();
+            i++;
+
         }
     }
 }
